50f43c8cf311a014e4107f83fad6640425f51808
LX-KI <rlaukeek@gmail.com>
2024-07-14 18:13:45 +0530

    print statements

---
+++ pr1.java
@@ -0,0 +1,16 @@
+/**
+ * pr1
+ */
+public class pr1 {
+
+    public static void main(String[] args) {
+        // System.out.println("hello World");
+        // System.out.println("hello wWorld");
+        System.out.print("*\n"); // use \n to print the next terms on the new line
+        System.out.println("**"); // use println to print the next terms on new line
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.print("******"); // use basic print statement to just print on the samr line
+    }
+}
