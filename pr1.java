05891d9ffcb6f56c19325e5377a4a6c659caa9d4
LX-KI <rlaukeek@gmail.com>
2024-07-14 21:04:14 +0530

    clean

--- pr1.java
+++ pr1.java
@@ -28,10 +28,10 @@ public class pr1 {
      * }
      * }
      */
- // scanner
-  /**
+  
+  /**scanner
    * 
-   * @param args
+   * 
      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the valur of a:");
@@ -43,44 +43,4 @@ public class pr1 {
         sc.close();
     }
 }
-import java.util.*;
-
-public class pr1 {
-    /**
-     * printsatements
-     * public static void main(String[] args) {
-     * // System.out.println("hello World");
-     * // System.out.println("hello wWorld");
-     * System.out.print("*\n"); // use \n to print the next terms on the new line
-     * System.out.println("**"); // use println to print the next terms on new line
-     * System.out.println("***");
-     * System.out.println("****");
-     * System.out.println("*****");
-     * System.out.print("******"); // use basic print statement to just print on the
-     * samr line
-     * }
-     * }
-     */
-    /**
-     * variables
-     * public static void main(String[] args) {
-     * int a = 223; // variable
-     * System.out.println(a);
-     * }
-     * }
-     */
-
-    // scanner
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the valur of a:");
-        int a = sc.nextInt();
-        System.out.println("enter the valur od b:");
-        int b = sc.nextInt();
-        int sum = a + b;
-        System.out.println("sum of a and b is:" + sum);
-    }
-}
-
-
-
+**/
