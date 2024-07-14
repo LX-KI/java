a70c978f174310cfc57d89de1754ec5900aa2f47
LX-KI <rlaukeek@gmail.com>
2024-07-14 19:08:35 +0530

    resolved issues

--- pr1.java
+++ pr1.java
@@ -1,11 +1,24 @@
 /**
  * pr1
  */
+
 public class pr1 {
-
+    /**
+     * public static void main(String[] args) {
+     * // System.out.println("hello World");
+     * // System.out.println("hello wWorld");
+     * System.out.print("*\n"); // use \n to print the next terms on the new line
+     * System.out.println("**"); // use println to print the next terms on new line
+     * System.out.println("***");
+     * System.out.println("****");
+     * System.out.println("*****");
+     * System.out.print("******"); // use basic print statement to just print on the
+     * samr line
+     * }
+     * }
+     */
     public static void main(String[] args) {
-        int a = 12;
+        int a = 223; // variable
         System.out.println(a);
-
     }
 }
