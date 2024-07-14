11f91a935b15645d6071211f9b51204731d76d6a
LX-KI <rlaukeek@gmail.com>
2024-07-14 21:35:08 +0530

    if statements

--- pr1.java
+++ pr1.java
@@ -28,19 +28,55 @@ public class pr1 {
      * }
      * }
      */
-  
-  /**scanner
-   * 
-   * 
-     public static void main(String[] args) {
+    /**
+     * scanner
+     * 
+     * 
+     * public static void main(String[] args) {
+     * Scanner sc = new Scanner(System.in);
+     * System.out.println("enter the valur of a:");
+     * int a = sc.nextInt();
+     * System.out.println("enter the valur od b:");
+     * int b = sc.nextInt();
+     * int sum = a + b;
+     * System.out.println("sum of a and b is:" + sum);
+     * sc.close();
+     * }
+     * }
+     **/
+    /**
+     * Conditional statements ex1
+     * public static void main (String[] args)
+     * {
+     * Scanner sc = new Scanner(System.in);
+     * System.out.println("Enter your age:");
+     * int age = sc.nextInt();
+     * if (age>18) {
+     * System.out.println("you are eligible to vote");
+     * }
+     * else {System.out.println("your are not an adult");
+     * }
+     * 
+     * sc.close();
+     * }
+     * }
+     */
+    /** Conditional statements ex2 */
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the valur of a:");
+        System.out.println("Enter the value of a:");
         int a = sc.nextInt();
-        System.out.println("enter the valur od b:");
+        System.out.println("Enter the value of b:");
         int b = sc.nextInt();
-        int sum = a + b;
-        System.out.println("sum of a and b is:" + sum);
+        if (a == b) {
+            System.out.println("a is equal to b");
+        } else if (a < b) {
+            System.out.println("a is less than b");
+        } else {
+            System.out.println("a is greater than b");
+
+        }
+
         sc.close();
     }
 }
-**/
