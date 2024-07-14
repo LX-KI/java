f2676aa07710c07dc3d441f5680442199b4e661e
LX-KI <rlaukeek@gmail.com>
2024-07-14 22:41:29 +0530

    switch

--- pr1.java
+++ pr1.java
@@ -61,22 +61,61 @@ public class pr1 {
      * }
      * }
      */
-    /** Conditional statements ex2 */
+    /**
+     * Conditional statements ex2
+     * public static void main(String[] args) {
+     * Scanner sc = new Scanner(System.in);
+     * System.out.println("Enter the value of a:");
+     * int a = sc.nextInt();
+     * System.out.println("Enter the value of b:");
+     * int b = sc.nextInt();
+     * if (a == b) {
+     * System.out.println("a is equal to b");
+     * } else if (a < b) {
+     * System.out.println("a is less than b");
+     * } else {
+     * System.out.println("a is greater than b");
+     * 
+     * }
+     * 
+     * sc.close();
+     * }
+     * }
+     */
+    /** switch statements */
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of a:");
         int a = sc.nextInt();
         System.out.println("Enter the value of b:");
         int b = sc.nextInt();
-        if (a == b) {
-            System.out.println("a is equal to b");
-        } else if (a < b) {
-            System.out.println("a is less than b");
-        } else {
-            System.out.println("a is greater than b");
-
+        System.out.println("Choose an operation:");
+        System.out.println("1: + (Addition)");
+        System.out.println("2: - (Subtraction)");
+        System.out.println("3: * (Multiplication)");
+        System.out.println("4: / (Division)");
+        System.out.println("5: % (Modulo)");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("result is :" + (a + b));
+                break;
+            case 2:
+                System.out.println("result is :" + (a - b));
+                break;
+            case 3:
+                System.out.println("result is :" + (a * b));
+                break;
+            case 4:
+                System.out.println("result is :" + (a / b));
+                break;
+            case 5:
+                System.out.println("result is :" + (a % b));
+                break;
+            default:
+                System.out.println("Invalid choice");
         }
-
         sc.close();
     }
 }
