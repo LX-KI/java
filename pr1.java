34ae07a89ae387921fb9101eb41d02e9c45cf89c
LX-KI <rlaukeek@gmail.com>
2024-07-16 23:17:00 +0530

    code

--- pr1.java
+++ pr1.java
@@ -82,40 +82,90 @@ public class pr1 {
      * }
      * }
      */
-    /** switch statements */
+    /**
+     * switch statements
+     * 
+     * public static void main(String[] args) {
+     * Scanner sc = new Scanner(System.in);
+     * System.out.println("Enter the value of a:");
+     * int a = sc.nextInt();
+     * System.out.println("Enter the value of b:");
+     * int b = sc.nextInt();
+     * System.out.println("Choose an operation:");
+     * System.out.println("1: + (Addition)");
+     * System.out.println("2: - (Subtraction)");
+     * System.out.println("3: * (Multiplication)");
+     * System.out.println("4: / (Division)");
+     * System.out.println("5: % (Modulo)");
+     * int choice = sc.nextInt();
+     * switch (choice) {
+     * case 1:
+     * System.out.println("result is :" + (a + b));
+     * break;
+     * case 2:
+     * System.out.println("result is :" + (a - b));
+     * break;
+     * case 3:
+     * System.out.println("result is :" + (a * b));
+     * break;
+     * case 4:
+     * System.out.println("result is :" + (a / b));
+     * break;
+     * case 5:
+     * System.out.println("result is :" + (a % b));
+     * break;
+     * default:
+     * System.out.println("Invalid choice");
+     * }
+     * sc.close();
+     * }
+     * }
+     */
 
-    public static void main(String[] args) {
+    /**
+     * LOOPS
+     * 
+     * 
+     * public static void main(String[] args) {
+     * 
+     * for (int i = 0; i <= 10; i++) {
+     * System.out.println(i);
+     * }
+     * }
+     */
+
+    /**
+     * While loop
+     * public static void main(String[] args) {
+     * int i = 0;
+     * while (i <= 10) {
+     * System.out.println(i);
+     * i++;
+     * }
+     * }
+     */
+
+    // PRINT THE SUM OF N NATURAL NO.
+
+   /*
+   public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of a:");
-        int a = sc.nextInt();
-        System.out.println("Enter the value of b:");
-        int b = sc.nextInt();
-        System.out.println("Choose an operation:");
-        System.out.println("1: + (Addition)");
-        System.out.println("2: - (Subtraction)");
-        System.out.println("3: * (Multiplication)");
-        System.out.println("4: / (Division)");
-        System.out.println("5: % (Modulo)");
-        int choice = sc.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("result is :" + (a + b));
-                break;
-            case 2:
-                System.out.println("result is :" + (a - b));
-                break;
-            case 3:
-                System.out.println("result is :" + (a * b));
-                break;
-            case 4:
-                System.out.println("result is :" + (a / b));
-                break;
-            case 5:
-                System.out.println("result is :" + (a % b));
-                break;
-            default:
-                System.out.println("Invalid choice");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
         }
-        sc.close();
+        System.out.println(sum);
     }
+} */ 
+   // print the table of a no , input by the user
+
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       for (int i = 1; i <= 10; i++) {
+           System.out.println(n + " * " + i + " = " + (n * i));
+       }
+
+   }
 }
