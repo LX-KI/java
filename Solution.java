c4084f71be594a5e6a34e841bed0dfd4bcb2a42b
LX-KI <rlaukeek@gmail.com>
2024-07-18 16:29:19 +0530

    sol

--- Solution.java
+++ Solution.java
@@ -14,6 +14,7 @@ class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
         int n = sc.nextInt();
         Solution sol = new Solution();
         sol.printSum(n);
