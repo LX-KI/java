8d8d93d5ceb944573e0797b1effce8fb5f217508
LX-KI <rlaukeek@gmail.com>
2024-07-19 23:12:14 +0530

    sclose

--- Greater_of_two.java
+++ Greater_of_two.java
@@ -1,23 +1,27 @@
+
 /**
  * Greater_of_two
  */
 import java.util.*;
+
 public class Greater_of_two {
 
-    int Greater(int a, int b){
-        if (a>b){
-            return a ;
+    int Greater(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
         }
-        else{return b;}
     }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number:");
         int a = sc.nextInt();
         System.out.println("Enter the second number:");
-        int b =sc.nextInt();
-        Greater_of_two gOf_two= new Greater_of_two();
-    System.out.println(gOf_two.Greater(a, b));        
-
+        int b = sc.nextInt();
+        Greater_of_two gOf_two = new Greater_of_two();
+        System.out.println(gOf_two.Greater(a, b));
+        sc.close();
     }
 }
