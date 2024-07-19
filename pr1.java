426abe8b9bc3e87d31cc760392cf648e4cde2aa1
LX-KI <rlaukeek@gmail.com>
2024-07-19 23:10:17 +0530

    prac_exer

--- pr1.java
+++ pr1.java
@@ -4,7 +4,7 @@
  */
 // import java.util.*;
 //import java.util.*;
-public class pr1 {
+//public class pr1 {
     /** printstatements
      * public static void main(String[] args) {
      * // System.out.println("hello World");
@@ -295,6 +295,30 @@ public class pr1 {
     }
 }
 */
+import java.util.Scanner;
+
+ class exercise {
+    double average(int a, int b, int c) {
+        double sum = (a + b + c) / 3;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number:");
+        int b = sc.nextInt();
+        System.out.println("Enter the third number:");
+        int c = sc.nextInt();
+        exercise ex = new exercise();
+        System.out.println("average of three numbers is:");
+        System.out.println(ex.average(a, b, c));
+        sc.close();
+    }
+}
+
+
 
 
 
