4d56bceb79262d3f4626d8ee860688c2606e573c
LX-KI <rlaukeek@gmail.com>
2024-07-21 20:59:30 +0530

    next

--- pr1.java
+++ pr1.java
@@ -295,28 +295,28 @@
     }
 }
 */
-import java.util.Scanner;
+// import java.util.Scanner;
 
- class exercise {
-    double average(int a, int b, int c) {
-        double sum = (a + b + c) / 3;
-        return sum;
-    }
+//  class exercise {
+//     double average(int a, int b, int c) {
+//         double sum = (a + b + c) / 3;
+//         return sum;
+//     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number:");
-        int a = sc.nextInt();
-        System.out.println("Enter the second number:");
-        int b = sc.nextInt();
-        System.out.println("Enter the third number:");
-        int c = sc.nextInt();
-        exercise ex = new exercise();
-        System.out.println("average of three numbers is:");
-        System.out.println(ex.average(a, b, c));
-        sc.close();
-    }
-}
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the first number:");
+//         int a = sc.nextInt();
+//         System.out.println("Enter the second number:");
+//         int b = sc.nextInt();
+//         System.out.println("Enter the third number:");
+//         int c = sc.nextInt();
+//         exercise ex = new exercise();
+//         System.out.println("average of three numbers is:");
+//         System.out.println(ex.average(a, b, c));
+//         sc.close();
+//     }
+// }
 
 
 
